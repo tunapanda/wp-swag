ba5733766d820fc0e1bf1809246e7c1438011751
Mikael Lindqvist <li.mikael@gmail.com>
2017-01-14 15:29:17 +0300

    testing RTD

---
+++ docs/_static/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","H5pSwagifact"],["c","H5pUtil"],["c","SettingsPageController"],["c","ShortcodeUtil"],["c","swag\\ArrayUtil"],["c","swag\\Singleton"],["c","swag\\WpUtil"],["c","SwagMapController"],["c","SwagPageController"],["c","Swagpath"],["c","SwagpathController"],["c","SwagpathSyncer"],["c","SwagPlugin"],["c","SwagPostItem"],["c","SwagTgmpaController"],["c","SwagTrack"],["c","SwagTrackController"],["c","SwagUser"],["c","Template"],["c","Xapi"]];
