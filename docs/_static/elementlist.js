3eebdca5a7fa4ca60accbc3e6fe9899a5f301705
Mikael Lindqvist <li.mikael@gmail.com>
2017-01-14 20:04:56 +0300

    use mkdocs

--- docs/_static/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","H5pSwagifact"],["c","H5pUtil"],["c","SettingsPageController"],["c","ShortcodeUtil"],["c","swag\\ArrayUtil"],["c","swag\\Singleton"],["c","swag\\WpUtil"],["c","SwagMapController"],["c","SwagPageController"],["c","Swagpath"],["c","SwagpathController"],["c","SwagpathSyncer"],["c","SwagPlugin"],["c","SwagPostItem"],["c","SwagTgmpaController"],["c","SwagTrack"],["c","SwagTrackController"],["c","SwagUser"],["c","Template"],["c","Xapi"]];
---
+++ docs/index.md
@@ -0,0 +1,6 @@
+hello world!
+
+is this a markdown
+doc or what?
+
+is the top stuff covered? WTF!
